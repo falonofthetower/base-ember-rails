768a6c6e66ab384d449ed9f7314e2f3ddd7612d6
Peter Karth <falonofthetower@gmail.com>
2016-11-26 19:40:05 -0500

    Ripped from the ember-simple-auth dummy

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+const { Router: EmberRouter } = Ember;
+
+const Router = EmberRouter.extend({
+  location: config.locationType
 });
 
 Router.map(function() {
+  this.route('login');
+  this.route('protected');
+  this.route('auth-error');
 });
 
 export default Router;
